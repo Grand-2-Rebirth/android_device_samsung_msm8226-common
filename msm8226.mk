90db3ef1ff02bf3a648c69e03e4c6d48bd45eaa1
Moaz Talaat <88838071+MoazSalem@users.noreply.github.com>
2025-05-10 18:35:03 +0300

    Fix Bluetooth audio and add Sound Trigger

--- msm8226.mk
+++ msm8226.mk
@@ -46,19 +46,15 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
-    android.hardware.audio.service \
-    android.hardware.audio@5.0 \
     android.hardware.audio@5.0-impl \
-    android.hardware.audio.common@5.0 \
-    android.hardware.audio.common@5.0-util \
-    android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@2.0-service \
-    android.hardware.audio.effect@5.0 \
+    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.soundtrigger@2.2-service \
     audio.a2dp.default \
     audio.bluetooth.default \
+    audio_policy.msm8226 \
     audio.primary.msm8226 \
     audio.r_submix.default \
     audio.usb.default \
