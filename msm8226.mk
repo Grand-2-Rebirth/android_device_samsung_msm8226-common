b7b50ab25dc1b881e7ff892333017ff7c81da365
Moaz Talaat <88838071+MoazSalem@users.noreply.github.com>
2025-05-11 11:55:19 +0300

    Better android.hardware.audio impl, add sound trigger

--- msm8226.mk
+++ msm8226.mk
@@ -46,16 +46,11 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
-    android.hardware.audio.service \
-    android.hardware.audio@6.0 \
     android.hardware.audio@6.0-impl \
-    android.hardware.audio.common@6.0 \
-    android.hardware.audio.common@6.0-util \
-    android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@2.0-service \
-    android.hardware.audio.effect@6.0 \
+    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@6.0-impl \
+    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.soundtrigger@2.2-service \
     android.hardware.bluetooth.a2dp@1.0-impl \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.bluetooth.default \
